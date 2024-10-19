cbc30358541ab6afb2dea295d19b1e341abc56e6
Christian Kirkegaard <christian@lowpoly.dk>
2024-10-19 02:12:31 +0200

    Init

---
+++ keys.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"github.com/charmbracelet/bubbles/key"
+)
+
+type keyMap struct {
+	Up     key.Binding
+	Down   key.Binding
+	Action key.Binding
+	Help   key.Binding
+	Quit   key.Binding
+}
+
+var keys = keyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "move up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "move down"),
+	),
+	Action: key.NewBinding(
+		key.WithKeys("enter", "space"),
+		key.WithHelp("↵", "select"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "esc", "ctrl+c"),
+		key.WithHelp("q", "quit"),
+	),
+}
+
+// ShortHelp returns keybindings to be shown in the mini help view. It's part
+// of the key.Map interface.
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Help, k.Quit}
+}
+
+// FullHelp returns keybindings for the expanded help view. It's part of the
+// key.Map interface.
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down, k.Action}, // first column
+		{k.Help, k.Quit},         // second column
+	}
+}
